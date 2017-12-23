c7724192edf66f15f2191f0a13114d827c265459
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2017-12-23 16:09:42 -0500

    Calendar now adjusts to view properly and automatically inserts months

--- js/adjust-calendar-view.js
+++ js/adjust-calendar-view.js
@@ -1,11 +1,18 @@
 
 $( document ).ready(function() {
 
-    // var second_bit = Math.ceil($("ul.calender>li.meeting").length / 2.5)
-    // var separate_meetings = $("ul.calender>li.meeting").slice(-6).remove()
+    var calendar = $(".schedule-container");
+    calendar.each(
+        function() {
+            var colOne = $(this).children().first();
 
-    // $(".first").after("<div class='col-xl second'></div>")
-    // $(".second").append("<ul class='calender'></ul>")
-    // separate_meetings.appendTo(".second>ul")
+            var l = colOne.children().first().children().length;
+            var val = Math.floor(l / 2.2);
+            var secondSet = colOne.children().first().children().slice(-val);
+    
+            colOne.after("<div class='col-xl'></div>");
+            colOne.next().append("<ul class='calender'></ul>");
+            secondSet.appendTo(colOne.next().children().first());
+    })
     
 })
