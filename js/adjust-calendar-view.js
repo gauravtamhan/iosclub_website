c020a3e7a3cf8dc5b6f4598fa96ec11821cff822
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2017-12-23 14:12:16 -0500

    init

---
+++ js/adjust-calendar-view.js
@@ -0,0 +1,11 @@
+
+$( document ).ready(function() {
+
+    // var second_bit = Math.ceil($("ul.calender>li.meeting").length / 2.5)
+    // var separate_meetings = $("ul.calender>li.meeting").slice(-6).remove()
+
+    // $(".first").after("<div class='col-xl second'></div>")
+    // $(".second").append("<ul class='calender'></ul>")
+    // separate_meetings.appendTo(".second>ul")
+    
+})
