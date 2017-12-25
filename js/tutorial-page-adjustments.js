26aa48facf1288593b25c188f65104e9f9e46f40
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2017-12-24 22:18:57 -0500

    tutorial drop down navbar works

--- js/tutorial-page-adjustments.js
+++ js/tutorial-page-adjustments.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
     var tag = $(".post-content").children().last().children();
-    $(".pill").append(tag)
+    $(".pill").append(tag);
+    $(".pill").children().text("Next");
 
 })
