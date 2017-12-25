9ba27c685e4f89d82ce786bfab0a0525e4e1351e
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2017-12-25 14:05:32 -0500

    made icon spin

--- js/tutorial-page-adjustments.js
+++ js/tutorial-page-adjustments.js
@@ -4,4 +4,16 @@ $( document ).ready(function() {
     $(".pill").append(tag);
     $(".pill").children().text("Next");
 
+    var clicked = false;
+    $(function() {
+        $("#expand-icon").click(function() {
+            clicked = !clicked;
+            if (clicked) {
+                $(this).children().addClass("clicked")
+            } else {
+                $(this).children().removeClass("clicked")
+            }
+        })
+    });
+
 })
