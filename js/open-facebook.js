b254b737ec9b232168b245a50e05ac91a7226eaa
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2018-02-12 11:43:04 -0500

    fixing fb link

--- js/open-facebook.js
+++ js/open-facebook.js
@@ -1,15 +1,23 @@
+---
+---
 
 function openFacebook() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    // var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    // var url = "{{ site.facebook_url }}";
+    // var appUri = "{{ site.facebook_ios_uri }}";
+
+    // if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    //     setTimeout(function () {
+    //         window.location = url;
+    //     }, 5);
+    //     window.location = appUri;
+    // } else {
+    //     window.open(url);
+    // }
+
     var url = "{{ site.facebook_url }}";
     var appUri = "{{ site.facebook_ios_uri }}";
 
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        setTimeout(function () {
-            window.location = url;
-        }, 5);
-        window.location = appUri;
-    } else {
-        window.open(url);
-    }
+    setTimeout(function () { window.location = url; }, 25);
+    window.location = appUri;
 }
