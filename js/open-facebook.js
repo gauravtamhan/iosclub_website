185972e3a7982bf1b6e04dc21c9a0270aaf3f0cf
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2018-02-12 11:45:28 -0500

    fb link

--- js/open-facebook.js
+++ js/open-facebook.js
@@ -2,22 +2,16 @@
 ---
 
 function openFacebook() {
-    // var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    // var url = "{{ site.facebook_url }}";
-    // var appUri = "{{ site.facebook_ios_uri }}";
-
-    // if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    //     setTimeout(function () {
-    //         window.location = url;
-    //     }, 5);
-    //     window.location = appUri;
-    // } else {
-    //     window.open(url);
-    // }
-
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
     var url = "{{ site.facebook_url }}";
     var appUri = "{{ site.facebook_ios_uri }}";
 
-    setTimeout(function () { window.location = url; }, 25);
-    window.location = appUri;
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        setTimeout(function () {
+            window.location = url;
+        }, 5);
+        window.location = appUri;
+    } else {
+        window.open(url);
+    }
 }
